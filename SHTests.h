29dd499ee527e74bb140e63fa9de1640978eda35
Sean Hamlin (韩翔) <Sean@vpn172022115025.its.yale.internal>
2014-05-21 16:11:37 -0400

    added a test class

---
+++ SHTests.h
@@ -0,0 +1,13 @@
+//
+//  SHTests.h
+//  Git Test
+//
+//  Created by Sean Hamlin (韩翔) on 5/21/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SHTests : NSObject
+
+@end
---
+++ SHTests.m
@@ -0,0 +1,13 @@
+//
+//  SHTests.m
+//  Git Test
+//
+//  Created by Sean Hamlin (韩翔) on 5/21/14.
+//
+//
+
+#import "SHTests.h"
+
+@implementation SHTests
+
+@end
